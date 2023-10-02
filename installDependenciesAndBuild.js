99e0b57f4d8f61d2d82382ba2454db97439f43ac
oscarfolha <1181600@isep.ipp.pt>
2023-10-02 17:56:36 +0100

    - a node script was created related with the issue #1 that runs the commands necessary for the dependency installations and building of the application. It displays all the messages that are returned from those commands.

---
+++ installDependenciesAndBuild.js
@@ -0,0 +1,37 @@
+const { exec } = require('child_process');
+
+
+function runCommand(command, successMessage) {
+  const childProcess = exec(command);
+
+  childProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  childProcess.stderr.on('data', (data) => {
+    console.error(data.toString());
+  });
+
+  childProcess.on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+  });
+
+  childProcess.on('close', (code) => {
+    if (code === 0) {
+      console.log(successMessage);
+    } else {
+      console.error(`Command exited with code ${code}`);
+    }
+  });
+}
+
+// Run the build script using npm run-script build
+runCommand('npm install', 'Dependencies installed successfully.');
+
+// Install dependencies using npm install
+runCommand('npm run-script build', 'Build script completed successfully.');
+
+
+
+
+
