f3be7a4013414359b2c09aa45bb969349e76aa06
Leandro <1190800@isep.ipp.pt>
2023-10-11 19:02:19 +0100

    adds getRecentPost features

--- public/app/src/modules/forum/components/posts/postRow/components/PostRow.tsx
+++ public/app/src/modules/forum/components/posts/postRow/components/PostRow.tsx
@@ -4,15 +4,22 @@ import "../styles/PostRow.sass"
 import { Post } from '../../../../models/Post';
 import { Points } from '../../points';
 import PostMeta from '../../post/components/PostMeta';
+import { PostFilterType } from '../../filters/components/PostFilters';
 
 interface PostRowProps extends Post {
   onUpvoteClicked: () => void;
   onDownvoteClicked: () => void;
   isLoggedIn: boolean;
+  filter: PostFilterType;
 }
 
 const PostRow: React.FC<PostRowProps> = (props) => (
-  <div className="post-row">
+  <div className="post-row" style={
+    {
+    backgroundColor: new Date(props.createdAt).toDateString() == new Date().toDateString() 
+    && props.filter == 'NEW' ? 'antiquewhite' : '#F9F9F9'
+  }
+    }>
     <Points
       onUpvoteClicked={() => props.onUpvoteClicked()}
       onDownvoteClicked={() => props.onDownvoteClicked()}
